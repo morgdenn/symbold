13678d63a806fb84af932ff5632b907cd71312f5
Morgan Dennithorne <morgdenn@gmail.com>
2016-05-16 22:30:25 +1000

    Update bookmark.js
    added numbers

--- bookmark.js
+++ bookmark.js
@@ -12,8 +12,19 @@ javascript:(function(){
 		[' from ', ' ↦ '],
 		[' with ', ' W/ '],
 		[' without ', ' W/O '],
-		[' (s|S)tar ', ' ☆ '],
-		[' percent ', ' % ']
+		[' star ', ' ☆ '],
+		[' percent ', ' % '],
+		[' one ', ' 1 '],
+		[' two ', ' 2 '],
+		[' three ', ' 3 '],
+		[' four ', ' 4 '],
+		[' five ', ' 5 '],
+		[' six ', ' 6 '],
+		[' seven ', ' 7 '],
+		[' eight ', ' 8 '],
+		[' nine ', ' 9 '],
+		[' ten ', ' 10 '],
+		[' eleven ', ' 11 '],
 	];
 
 
